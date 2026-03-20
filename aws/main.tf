2bbe00f5b792ceba7b676a9a112c5052c36e412a
JulioCVaz <julio.oliveiravaz0@gmail.com>
2026-03-19 23:06:04 -0300

    Update S3 backend key with specific prefix to avoid state conflicts

--- aws/main.tf
+++ aws/main.tf
@@ -8,7 +8,7 @@ terraform {
   }
 
   backend "s3" {
-    key     = "terraform-data.tfstate" # Different key to avoid conflicts
+    key     = "tech-challenge-data/terraform.tfstate"
     region  = "us-east-1"
     encrypt = true
   }
